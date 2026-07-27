51c2bcdbdf8da269c5bfdcd5cb6ea67b5b87b08b
Cacerez Blacio Johan Isaac (CeaP/MFP8.1) <caj3cea@bosch.com>
2026-07-27 14:10:08 -0600

    tailiwd: diseñando card

--- src/components/organism/ProcessCard.tsx
+++ src/components/organism/ProcessCard.tsx
@@ -1,13 +1,24 @@
 import { Icon } from "../atoms/Card/Icon";
 import { Title } from "../atoms/Title";
+import bgPickAndPlace from "../../assets/Bg_images/PickAndPlace.png";
 
 export const ProcessCard = () => {
   return (
     <div>
-      <section>
-        <Icon />
-        <Title text="Pick and Place" level="h2" />
+      {/* 
+        1. Usamos 'style' para inyectar de forma segura la ruta dinámica que Vite genera.
+        2. Usamos 'className' con clases estándar de Tailwind para el diseño y comportamiento.
+      */}
+      <section
+        className="bg-cover bg-center bg-no-repeat p-6 min-h-40 flex flex-row"
+        style={{ backgroundImage: `url(${bgPickAndPlace})` }}
+      >
+        <div className="bg-white flex rounded-2xl p-4 items-center gap-4">
+          <Icon />
+          <Title text="Pick and Place" level="h2" />
+        </div>
       </section>
+
       <section>
         <div>
           <Title text="Subtitulo de proceso" level="h3" />
