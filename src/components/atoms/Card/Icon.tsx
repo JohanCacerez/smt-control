c99ed6e93b2e03b983767b48a41a6905bf644194
Cacerez Blacio Johan Isaac (CeaP/MFP8.1) <caj3cea@bosch.com>
2026-07-27 14:56:50 -0600

    card: se añaden efectos a la tarjeta

--- src/components/atoms/Card/Icon.tsx
+++ src/components/atoms/Card/Icon.tsx
@@ -3,7 +3,7 @@ import AoiIcon from "../../../assets/Icons_process/AOI.png";
 
 export const Icon = () => {
   return (
-    <div className="flex items-center justify-center w-24 h-24">
+    <div className="flex items-center justify-center w-12 h-12">
       <img
         className="rounded-xl"
         src={AoiIcon}
--- src/components/organism/ProcessCard.tsx
+++ src/components/organism/ProcessCard.tsx
@@ -4,28 +4,23 @@ import bgPickAndPlace from "../../assets/Bg_images/PickAndPlace.png";
 
 export const ProcessCard = () => {
   return (
-    <div>
-      {/* 
-        1. Usamos 'style' para inyectar de forma segura la ruta dinámica que Vite genera.
-        2. Usamos 'className' con clases estándar de Tailwind para el diseño y comportamiento.
-      */}
+    <div className="max-w-md rounded-t-2xl bg-gray-400 overflow-hidden">
       <section
-        className="bg-cover bg-center bg-no-repeat p-6 min-h-40 flex flex-row"
+        className="bg-cover bg-center rounded-t-2xl bg-no-repeat p-6 py-10 min-h-40 flex flex-row items-center"
         style={{ backgroundImage: `url(${bgPickAndPlace})` }}
       >
-        <div className="bg-white flex rounded-2xl p-4 items-center gap-4">
+        <div className="bg-white/02 backdrop-blur-sm border border-white/20 flex rounded-2xl p-2 items-center gap-2 shadow-lg">
           <Icon />
-          <Title text="Pick and Place" level="h2" />
+          <Title text="Pick and Place" level="h3" />
         </div>
       </section>
 
-      <section>
+      <section className="p-6 flex flex-col gap-4">
         <div>
-          <Title text="Subtitulo de proceso" level="h3" />
-          <Title text="Descripción del proceso" level="h4" />
+          <Title text="Descripción del proceso" level="h5" />
         </div>
         <div>
-          <Title text="Personal a cargo" level="h4" />
+          <Title text="Personal a cargo" level="h5" />
           <p>Persona 1</p>
           <p>Persona 2</p>
           <p>Persona 3</p>
