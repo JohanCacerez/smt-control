58e322780eb9c9c1c7d6b224696a8e97e05d94a5
Cacerez Blacio Johan Isaac (CeaP/MFP8.1) <caj3cea@bosch.com>
2026-09-24 10:50:14 -0600

    rutas de procesos

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,24 @@ function App() {
           <Route path="settings" element={<SettingsPage />} />
           <Route path="user" element={<UserPage />} />
           <Route path="*" element={<NotFoundPage />} />
+
+          <Route
+            path="pick-and-place"
+            element={<h1>Página de Pick and Place</h1>}
+          />
+          <Route path="aoi" element={<h1>Página de AOI</h1>} />
+          <Route
+            path="spp"
+            element={<h1>Página de Inspección de Pasta de Soldadura</h1>}
+          />
+          <Route path="spi" element={<h1>Página de Inspección de SPI</h1>} />
+          <Route path="oven" element={<h1>Página de oven</h1>} />
+          <Route path="axi" element={<h1>Página de axi</h1>} />
+          <Route path="laser" element={<h1>Página de laser</h1>} />
+          <Route path="clinching" element={<h1>Página de clinching</h1>} />
+          <Route path="conveyor" element={<h1>Página de conveyor</h1>} />
+          <Route path="hotcaulking" element={<h1>Página de hotcaulking</h1>} />
+          <Route path="ict" element={<h1>Página de ict</h1>} />
         </Route>
       </Routes>
     </>
